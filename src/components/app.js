bb34da5d14d2b83e84b2b220e4bcb84070f64508
Thanos Eleftherakos <man_or_astroman@MacBook-Air.local>
2016-06-05 10:51:11 +0300

    inital commit

---
+++ src/components/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+require('./test.scss');
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="test">React simple starters</div>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loaders: ['react-hot','babel-loader']
+    },
+    {
+      test: /\.scss$/,
+      loaders:['style','css','sass']
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
